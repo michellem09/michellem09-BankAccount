d31c9fdc685d110a1780e6bec0f2c71c0ac0bb14
Michelle Mendez <mmendez5584@gmail.com>
2023-02-10 08:43:20 +0000

    Bank Account Lab

--- src/main/java/BankService.java
+++ src/main/java/BankService.java
@@ -23,7 +23,7 @@ public class BankService {
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
-
+        balance = balance + amount;
     }
 
     /**
@@ -33,6 +33,10 @@ public class BankService {
      */
     public void withdraw(double amount){
 
+        if(amount <= balance){
+            balance = balance - amount;
+        }
+        
     }
 
     /**
@@ -40,6 +44,6 @@ public class BankService {
      * @return the user's balance.
      */
     public double getBalance(){
-        return 0;
+        return balance;
     }
 }
